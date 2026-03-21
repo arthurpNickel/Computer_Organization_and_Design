e36394c5e06ffb1ff7f085ff029fd008bcd5d42a
Arthur Paul Nickel <apn25@inf.ufpr.br>
2026-03-21 16:22:58 -0300

    [add] start of work

---
+++ T1/T1.asm
@@ -0,0 +1,34 @@
+#colocar outras coisas aqui
+
+A: .word 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 #vetor A
+B: .word 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 #vetor B
+R: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 #vetor R zerado -> será que usar space dá certo????
+
+.globl main
+
+.text
+
+
+
+main:
+    
+	#inicializar vetores??
+	
+	#inicia iterador
+	
+	#chama função de soma_vetor
+	
+	#soma rótulos
+	
+	#vertifica se chegou ao fim
+	
+soma_vetor:
+	
+	#soma rótulos
+	
+	#incrementa iterador
+	
+	#vertifica se chegou ao fim
+		#se sim, chama soma_vetor novamente
+		#se não, retorna para o main
+	
