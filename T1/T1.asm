3403c1e8005e0e036616414613d46614045debac
Arthur Paul Nickel <apn25@inf.ufpr.br>
2026-03-28 10:37:36 -0300

    [fix] Converting the syntax from RV32 to REDUX-V in the EGG simulator

--- T1/T1.asm
+++ T1/T1.asm
@@ -8,6 +8,7 @@
 
 .data
 #QUESTION: word mesmo?
+#QUESTION: Dá para fazer isso no EGG?
 A: .word 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 #vetor A
 B: .word 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 #vetor B
 R: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 #vetor R zerado -> QUESTION: será que usar space dá certo?
@@ -18,22 +19,26 @@ main:
     
 	#QUESTION: inicializar vetores?
 	
-	#TO DO: inicia iterador
+	#Inicia iterador
 	#QUESTION: Função específica?
-	addi a4, zero , 0 #QUESTION: inicia em 0???
+	addi a4, zero , 0
 	
 	#QUESTION:Ou recebe 10? -> depende se vai ter multiplicação
+	#QUESTION:Quantos bytes tem 1 inteiro?
 	addi a0, zero, 40	#a0(tam) = 10(tam do vetor) * 4(tam dos números, em bytes)
 	
 	#Atribui a ponteiros o endereço dos vetores
-	#QUESTION: Pode usar essa pseudo-instrução? -> ACHO QUE NÃO
-	la a1, A		#a0 = &A[0]
-	la a2, B		#a1 = &B[0]
-	la a3, R		#a3 = &R[0]
+	#QUESTION: Usar addi mesmo?
+	addi a1, A		#a0 = &A[0]
+	addi a2, B		#a1 = &B[0]
+	addi a3, R		#a3 = &R[0]
 	
-	#TO DO: chama função de soma_vetor
+	#Chama função de soma_vetor
+	#QUESTION: Soam mesmo ou só desvia?
+	#QUESTION: soma_vetor ou soma e imprime?
+	ji soma_vetor
 	
-	#TO DO: imprime R -> QUESTION: função para isso?
+	#TO DO: imprime R -> QUESTION: função para isso? -> Dá para ser aqui? Ou função chamada por soma_vetor?
 		#inicia iterador?
 		#percorre vetor até .. imprimindo?
 	
@@ -44,14 +49,22 @@ soma_vetor:
 	sub t0, a0, a4		#t0 = tam - i
 	brzr t0, fim		# if (i == tam) return
 
-	#QUESTION: Será que não dá para declarar ponteiro de R aqui?
+	#QUESTION: Será que não dá para declarar ponteiro de R aqui? -> Ou função de inicialização?
+	
+	#Pega inteiros na memória
+	ld t1, a1 		#a1 = A[i]
+	ld t2, a2		#a2 = B[i] 
 	
 	#Soma rótulos
-	add a3, a0, a1		#R[i] = A[i] + B[i]
+	add t3, t1, t2		#t3 = A[i] + B[i]
+	
+	#QUESTION: verificar se está certo a ordem
+	st a3, t3		#R[i] = t3
 	
 	#Incrementa iterador
 	#QUESTION: Função específica?
 	#QUESTION: Será que não dá para usar multiplicação?
+	#QUESTION: Soma 1 ou 4? -> Depende da quantidade de bytes de int
 	addi a4, a4, 4		#i += 4
 	
 	#QUESTION:Será que branch aqui? -> economiza 3 operações de soma
