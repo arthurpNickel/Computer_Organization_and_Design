f17401fc6d754041d44d00fe54ace6ca43280a2f
Arthur Paul Nickel <apn25@inf.ufpr.br>
2026-04-01 14:44:24 -0300

    [fix] Cleaned the code

--- T1/T1_V3.asm
+++ T1/T1_V3.asm
@@ -6,112 +6,113 @@
 
 ; TODO: Ver se dá para imprimir com REDUX
 ; TODO: Ver minha nova lógica tá dando boa e responder question, com o chat
-; QUESTION: Será que dá para fixar um registrador com o endereço de A -> fazer calculo de endereços sempre com base nele
 ; TODO: Verificar se dá para colocar labels nos vetores
 
-; Q: .text?
+; QUESTION: Será que dá para fixar um registrador com o endereço de A -> fazer calculo de endereços sempre com base nele
+; QUESTION: inicializar vetores no main?
+; QUESTION: Colocar fim no meio é isso é gambiarra?
+; QUESTION: Dá para fazer returns?
+; QUESTION: Fazer função inicializadora antes de soma_vetor?
+	; E colocar sub r3, r3
+; QUESTION: Função de inicia iterador? Função de percorre? Dá?
+; QUESTION: O uso do brzr está certo
+; QUESTION:	Usar tam constante? (10)
+; QUESTION: .bits8 mesmo?
+; QUESTION: será que usar .space no vetor R dá certo? -> acho melhor garantir zerado
 
-main:
-    
-	; QUESTION: inicializar vetores?
-	
+main:	
 	; Inicia iterador
-	; QUESTION: Função específica? Dentro de função inicializadora?
 	sub r3, r3		; r3 (i) = 0
 	
-	; Chama função de soma_vetor
-	; QUESTION: Soma mesmo ou só desvia?
-	; QUESTION: soma_vetor ou soma e imprime?
-	ji soma_vetor
-	
-	; TO DO: imprime R -> QUESTION: função para isso? -> Dá para ser aqui? Ou função chamada por soma_vetor?
-		; inicia iterador?
-		; percorre vetor até .. imprimindo?
+	; Desvia para soma_vetor
+	ji soma_vetor	; ESSE JI TA MUITO ESTRANHO!!!!!!!!!!!!!!!
 
-; QUESTION: Isso é gambiarra?
 fim:
-	; QUESTION: return??? -> PArece que não dá
 	ebreak
 	
-soma_vetor:
-	; QUESTION: É assim que usa brzr?
-	; QUESTION: Aqui é a posição certa do branch -> pode ser entre incrementa iterador e percorre vetores
-		; Aqui impede soma de vetores vazios -> mas nunca vai ter
-	
-	; ìf (i == tam) desvie para "imprime"
+soma_vetor:	
+	; if (i == tam) desvie para 'fim'
+
     ; r1(aux) = tam - i
 	sub r0, r0
-	addi 7		; QUESTION: 40 ou 10? Vai ter que ser esse número constante mesmo?
-	addi 3
+	addi 7
+	addi 3				; r0 = tam
 	sub r0, r3			; r0 = tam - i
     sub r1, r1          
     add r1, r0          ; r1 = r0 (tam - i)
 
 	sub r0, r0
-    addi fim            ; r0 = & de label "fim" ; ACHO QUE ISSO TA DANOD ERRADO -> colocou "0xfe"
+    addi fim            ; r0 = &ebreak
 
-	brzr r1, r0			; se r1 == 0; branch para R[r0]
+	brzr r1, r0			; se r1 == 0, desvie para 'fim'
 	; -----------------------------------
+	; Soma de rótulos e armazenamento
+	; -----------------------------------
+	; Calcula &A[i] (endereço base para vetores) e pega A[i]
 
-	; r1 = A[i]
-    ; Calcula endereço do próximo inteiro
-
+	; !!!!!!!!!!!!!!!!!
 	; r0 = i
 	sub r0, r0
 	add r0, r3
 
 	; AJUSTE DE ENDEREÇO!!!!!!!!!!!!!!!!!!!!
-	; r0 = i + A
+	; r0 = i + A (pos 35)
     addi 7
 	addi 7
     addi 7
 	addi 7
-	addi 3
+	addi 7
 
-	; r1 = A[i]
+	; r1 = A[i] - Armazena em r1
 	ld r1, r0		; (Importante: A[i] sobrescreve o que tem em r1)
 
 	; ----------------------------------
+	; Calcula &B[i] e pega B[i]
 
-	; r2 = B[i]
-	; endereço de B[i] em r0: r0 = (A + i) + 10 + número de instruções adicionadas = B + i
-	
-	; r0 = r0(A+i) + 10 + n
+	; r0 = r0 (A+i) + 10 (tam de A)
 	addi 7			
-	addi 3 ; + n (se add mais um tirando essa)
+	addi 3
 
 	; r2 = B[i]
 	ld r2, r0
 
 	; ---------------------------------
+	; Calcula &R[i] e armazena soma de rótulos
+
+	; r0 = B + i + 10 = R + i
+    addi 7
+	addi 3
 
 	; r1 = A[i] + B[i]
 	add r1, r2
 
-
-	; R[i] = r1 (A[i] + B[i])
-
-	; r0 = i + R = (B + i) + 10 + número de instruções adicionadas
-	; r0 = B + i
-    addi 7
-	addi 3 ; + n (se add mais uma tirando essa)
-
-    ; QUESTION: Tá certo a ordem de registradores?
-    st r1, r0       ; M[r0] = R[r1] <-> R[i] = A[i] + B[i]
+	; R[i] = A[i] + B[i]
+    st r1, r0       ; M[r0] = R[r1]
 	
-	; Incrementa iterador
-	; QUESTION: Função específica? Dá?
-	; i = i + 1
+	;------------------------------------
+	; Incrementa iterador - i++
+
 	sub r0, r0
 	addi 1
 	add r3, r0
-	
-	; QUESTION: Assim que usa ji?? 
-	ji soma_vetor		; goto soma_vetor -> TODO: Verificar comentário
 
-; QUESTION: word mesmo?
-; QUESTION: Dá para fazer isso no EGG?
-; QUESTION: .8 mesmo?
-.bits8 0x00 0x02 0x04 0x06 0x08 0x0A 0x0C 0x0E 0x10 0x12 ; vetor A
-.bits8 0x01 0x03 0x05 0x07 0x09 0x0B 0x0D 0x0F 0x11 0x13 ; vetor B
-.bits8 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 ; vetor R-> QUESTION: será que usar space dá certo?
+	;------------------------------------	
+	; Desvia para soma_vetor usando brzr
+
+	sub r0, r0
+	addi soma_vetor
+
+	sub r1, r1		; Força desvio
+	brzr r1, r0	
+
+;----------------------------------------
+;Vetores:
+
+; Vetor A (pos 34):
+.bits8 0x00 0x02 0x04 0x06 0x08 0x0A 0x0C 0x0E 0x10 0x12
+
+; Vetor B (pos 44):
+.bits8 0x01 0x03 0x05 0x07 0x09 0x0B 0x0D 0x0F 0x11 0x13
+
+; Vetor R (pos 54):
+.bits8 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
---
+++ T1/T1_V4.asm
@@ -0,0 +1,118 @@
+; O aluno deverá escrever, em Assembly de REDUX-V, um programa que some dois vetores de 10 posições
+; fazendo R=A+B. O código deve inicializar os vetores A, B e R. Os vetores devem iniciar logo após as
+; instruções na memória (note que REDUX-V é uma arquitetura Von Neumann). Você deve implementar a
+; soma com loop para percorrer o vetor. A lista de instruções é dada na próxima página. O Assembly
+; deve ser escrito no formato legível pelo emulador EGG (https://github.com/gboncoffee/egg).
+
+; TODO: Ver se dá para imprimir com REDUX
+; TODO: Ver minha nova lógica tá dando boa e responder question, com o chat
+; QUESTION: Será que dá para fixar um registrador com o endereço de A -> fazer calculo de endereços sempre com base nele
+
+; Q: .text?
+
+main:
+    
+	; QUESTION: inicializar vetores?
+	
+	; Inicia iterador
+	; QUESTION: Função específica? Dentro de função inicializadora?
+	sub r3, r3		; r3 (i) = 0
+
+	; Pega o endereço de A para usar como base
+	; r0 = A
+	sub r0, r0
+    addi 7
+	addi 7
+    addi 7
+	addi 7
+	addi 5
+	; r2 = A
+	sub r2, r2
+	add r2, r0
+	
+	; Chama função de soma_vetor
+	; QUESTION: Soma mesmo ou só desvia?
+	; QUESTION: soma_vetor ou soma e imprime?
+	ji soma_vetor
+	
+	; TO DO: imprime R -> QUESTION: função para isso? -> Dá para ser aqui? Ou função chamada por soma_vetor?
+		; inicia iterador?
+		; percorre vetor até .. imprimindo?
+
+soma_vetor:
+	; QUESTION: É assim que usa brzr?
+	; QUESTION: Aqui é a posição certa do branch -> pode ser entre incrementa iterador e percorre vetores
+		; Aqui impede soma de vetores vazios -> mas nunca vai ter
+	
+	; ìf (i == tam) desvie para "imprime"
+    ; r1(aux) = tam - i
+	sub r0, r0
+	addi 7		; QUESTION: 40 ou 10? Vai ter que ser esse número constante mesmo? PQ MUDOU PARA 0xfa??????????????
+	addi 3
+	sub r0, r3			; r0 = tam - i
+    sub r1, r1          
+    add r1, r0          ; r1 = r0 (tam - i)
+
+	sub r0, r0
+    addi fim            ; r0 = & de label "fim"
+
+	brzr r1, r0
+	; -----------------------------------
+
+	; r1 = A[i]
+    ; Calcula endereço do próximo inteiro
+	sub r1, r1		; r1 = 0
+	add r1, r3		; r1 = i
+	add r1, r2		; r1 = i + A
+
+	; r0 guarda endereço de A[i]
+	sub r0, r0
+	add r0, r1
+
+	ld r1, r0		; r1 = A[i] (Importante: A[i] sobrescreve o que tem em r1)
+
+	; r0 = B[i]
+	; r0 = A[i] + 10 + número de instruções adicionadas = B[i]
+	
+	; r0 += 10 + n
+	addi 7			
+	addi 3 ; + n (se add mais um tirando essa)
+
+	ld r0, r0		; r0 = B[i]		; QUESTION: Pode isso arnaldo??
+
+	; QUESTION: Sobrou um registrador aqui! Ent será que dá para fazer de outro jeito?
+	add r1, r0		; r1 = A[i] + B[i]
+
+	; R[i] = r2 (A[i] + B[i])
+
+	; r0 = i + &R[0] = B[i] + 10 + número de instruções adicionadas
+    addi 7
+	addi 3 ; + n (se add mais uma tirando essa)
+
+    ; QUESTION: Tá certo a ordem de registradores?
+    st r1, r0       ; M[r0] = R[r1] <-> R[i] = A[i] + B[i]
+	
+	; Incrementa iterador
+	; QUESTION: Função específica? Dá?
+
+	; i = i + 1
+	sub r0, r0
+	addi 1
+	add r3, r0
+	
+	; QUESTION: Assim que usa ji?? 
+	ji soma_vetor		; goto soma_vetor -> TODO: Verificar comentário
+
+; imprime:
+	; TODO: Ver se dá para imprimir no REDUX
+
+fim:
+	; QUESTION: return??? -> PArece que não dá
+	ebreak
+
+; QUESTION: word mesmo?
+; QUESTION: Dá para fazer isso no EGG?
+; QUESTION: .8 mesmo?
+.bits8 0x00 0x02 0x04 0x06 0x08 0x0A 0x0C 0x0E 0x10 0x12 ; vetor A
+.bits8 0x01 0x03 0x05 0x07 0x09 0x0B 0x0D 0x0F 0x11 0x13 ; vetor B
+.bits8 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 ; vetor R-> QUESTION: será que usar space dá certo?
