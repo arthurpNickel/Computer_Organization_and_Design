80fd5d1b123791be93060bf3252bfc3ac52c8d7b
Arthur Paul Nickel <apn25@inf.ufpr.br>
2026-06-05 18:16:23 -0300

    [mod] I reviewed the code

--- T3/soma_vetores.asm
+++ T3/soma_vetores.asm
@@ -8,55 +8,75 @@
 # B={20,21,22,23,24,25,26,27,28,29,30,31}.
 # No caso do processador vetorial os dados estarão distribuidos nas memórias dos PEs
 
+#DECISÕES (D)
+    # 1: Antes de usar, todos os registradores foram zerados antes
+    # 2: Mesmo que eu saiba que o dado está zerado, eu vou zerá-lo de novo por precaução
+    # 3: inicialização dos dados do segundo vetor!!!!!!!!!!!!!!!!!
+    # 4: Considerei que os endereços chegam certos em ... para a inicialização do segundo vetor
+    # 5: Considerei que o vr1 se manterá com constante 4 na inicialização dos 2 vetores
+    # 6: Não desviarei para labels que estão abaixo da label atual
+    # 7: Load/Store não mexem no endereço de acesso a memória
 
-# SE LOAD/STORE NÃO INTERFERIR NO ENDEREÇO
+#QUESTIONS
+    # Inicializo os dados do segundo vetor de qual forma?
+
 main:
-    # Vai algo aqui? Ou seria igual o inicializa 1?
+    # Q: Vai algo aqui? Ou seria igual o inicializa 1?
 
 inicialilza_1:
     # Inicializa iterador -------------------
 
     # Inicializa "n" (número de iterações) em sr3
-    # s.movh 0 ?
+    s.movh 0            #D1
     s.movl 3
-    s.sub sr3, sr3      # Q: Precisa?
+    s.sub sr3, sr3      #D1
     s.add sr3, sr1      # sr3 = 3
 
     # Inicializa endereços de jump --------------
 
     # sr2 recebe endereço do inicializa_2
+    s.movl X
+    s.movl Y
+    s.sub sr2, sr2      #D1
+    s.add sr2, sr3
 
     # sr1 recebe endeço de inicializa_vetores
+    s.movl X
+    s.movl Y
 
     # Inicializa dados iniciais -----------------
-    v.sub vr2, vr2      # vr2 = {0, 0, 0, 0} -> Q: Precisa?
+    v.sub vr2, vr2      # vr2 = {0, 0, 0, 0} - D1
     v.add vr2, vr0      # vr2 = {0, 1, 2, 3} - dados iniciais
 
     # Inicializa endereços iniciais  ------------
-    v.sub vr3, vr3      # vr3 = {0, 0, 0, 0} -> Q: Precisa?
+    v.sub vr3, vr3      # vr3 = {0, 0, 0, 0} - D1
     v.add vr3, vr0      # vr3 = {0, 1, 2, 3} - endereços iniciais
 
     # Define vr1 com a constante 4 --------------
-    v.movh 0
-    v.movl 4        # Nesse caso em específico essa instrução é inútil
+    v.movh 0            #D1
+    v.movl 4
 
     # Desvia para "inicializa_vetores"
-    s.brzr sr0, sr1       # Q: Precisa? -> provavelmente
+    s.brzr sr0, sr1
 
 inicializa_2:
     # Inicializa iterador -----------------------
 
     # Inicializa "n" (número de iterações) em sr3 
-    # s.movh 0 ?
+    s.movh 0            # D2
     s.movl 3
-    s.sub sr3, sr3      # Q: Precisa?
+    s.sub sr3, sr3      # D2
     s.add sr3, sr1      # sr3 = 3
 
     # Inicializa endereços de jump ---------------
 
     # sr2 recebe endereço do próximo label
+    s.movl X
+    s.movl Y
+    s.sub sr2, sr2
+    s.add sr2, sr3
 
-    # sr1 recebe endeço de inicializa_vetores
+    # D6
 
     # Inicializa dados iniciais - 20 (10100) -----
     v.movl 4
@@ -67,59 +87,53 @@ inicializa_2:
     v.add vr2, vr0      # vr2 = {20, 21, 22, 23} - dados iniciais
 
 #OU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # Considerando que vr2 chega com {12, 13, 14, 15}
+    # Considerando que vr2 chega com {12, 13, 14, 15} e vr1 com {4, 4, 4, 4}
     v.add vr2, vr1      # vr2 = {16, 17, 18, 19}
     v.add vr2, vr1      # vr2 = {20, 21, 22, 23}
 
-    #Q: Precisa inicializar endereços iniciais? Ou já vem prontos da inicialização do primeiro vetor?!!!!!!!!!!!
-        # Acho que vr3 = {12, 13, 14, 15} depois da inicialização do primeiro vetor, então n precisa fazer algo
+    # D4
 
-    # Define vr1 com a constante 4 -> Q: Se não mudar da inicialização 1 para a 2 não precisa!
-    v.movh 0
-    v.movl 4        # Nesse caso em específico essa instrução é inútil
+    # D5
 
-    # Desvia para "inicializa_vetores"
-    s.brzr sr0, sr2       # Q: Precisa? -> provavelmente
+    # D6
 
 inicializa_vetores:
-    # Estou considerando que endereço está em VR3!!!
+    # Estou considerando que endereço está em VR3, dados em VR2 e 4 em VR1 !!!
 
-    v.st vr2, vr3   # Guarda dados na memória
+    v.st vr2, vr3       # Guarda dados na memória
 
-    v.add vr2, vr1  # vr2 = vr2 + vr1 / vr2 += 4 -> itera dados - estão em sequência
-    v.add vr3, vr1  # vr3 = vr3 + vr1 / vr3 += 4 -> itera endereço - !!!!!!!!!!!!!!!
+    v.add vr2, vr1      # vr2 = vr2 + vr1 / vr2 += 4 -> itera dados - estão em sequência
+    v.add vr3, vr1      # vr3 = vr3 + vr1 / vr3 += 4 -> itera endereço - !!!!!!!!!!!!!!!
 
     # "if (i == 3) finaliza loop"
-    #s.movh 0 ?
+    s.movh 0            # D2
     s.movl 1            # Inicializa variavel de iteração
-    s.add sr3, sr1        # n--
+    s.add sr3, sr1      # n--
 
-    s.brzr sr3, sr2       # Se sr3 == 0 -> pula para ...
+    s.brzr sr3, sr2     # Se sr3 == 0 -> pula para próximo label
 
     # Se não...
     s.movh Y            # Monta endereço de "inicializa_vetores"
     s.movl X
 
-    s.brzr sr0, sr1       # Desvia para "inicializa_vetores"
+    s.brzr sr0, sr1     # Desvia para "inicializa_vetores"
 
-# Q: Esse nome mesmo?
-chama_soma:
+soma_setup:
     # Inicializa iterador
 
     # Inicializa "n" (número de iterações) em sr3 
-    # s.movh 0 ?
+    s.movh 0            # D2
     s.movl 3
-    s.sub sr3, sr3      # Q: Precisa?
+    s.sub sr3, sr3      # D2
     s.add sr3, sr1      # sr3 = 3
 
-    # Q: Inicializa endereço de fim -> se for em baixo não precisa
-        # Posso usar epaço para colocar constante 1 (decremento) e outro para endereço de loop
+    # Q: Inicializa endereço de fim -> Vale a pena?
+        # Posso usar espaço para colocar constante 1 (decremento) e outro para endereço de loop
     
     s.movh X            # sr1 = &fim
     s.movl Y
     s.sub sr2, sr2      # sr2 = 0
     s.add sr2, sr1      # sr2 = sr1
-
 
     # Inicializa endereços iniciais
     v.sub vr3, vr3      # vr3 = {0, 0, 0, 0}
@@ -128,34 +142,33 @@ chama_soma:
 soma_vetores:
     v.ld vr2, vr3       # Carrega 1/3 do vetor 1 em vr2
 
-    # Monta endereço do vetor 2
-    v.movh 0            # vr1 = 8
-    v.movl 8
+    # Monta endereço do vetor 2 em vr1
+    v.movh 0            # vr1 = 12
+    v.movl 12
     v.add vr3, vr1
 
     v.ld vr1, vr3       # Carrega 1/3 do vetor 2 em vr1
 
-    v.add vr2, vr3      # Soma vetor 1 e 2 e armazena em vr2
+    v.add vr2, vr1      # Soma vetor 1 e 2 e armazena em vr2
 
-    # Monta endereço do vetor resultado
+    # Monta endereço do vetor resultado em vr1
     v.movh 0
-    v.movl 8
+    v.movl 12
     v.add vr3, vr1
 
     v.st vr2, vr3       # Guarda vetor resultado
 
     # "if (i == 3) finaliza programa"
-    # s.movh 0 ?
+    s.movh 0            # D2
     s.movl 1
     s.sub sr3, sr1
 
-    s.brzr vr3, vr2
+    s.brzr sr3, sr2
 
     # Se não...
-
     # Monta próximo endereço base
-    # v.movh 0 ?
-    v.movl 12
+    # t: ARRUMAR AQUI!!!
+    v.movl 12           # Considerei a parte de cima zerada por 3 instruções atrás
     v.sub vr3, vr1      #vr3 = vr3 - 12 (diminuiu 16 endereços para voltar ao vetor 1 e soma 4 para ir para o próximo bloco)
 
     # Monta endereço de soma_vetores
@@ -165,6 +178,9 @@ soma_vetores:
 
     s.brzr sr0 sr1
 
-# Q: SE LOAD/STORE INTERFERIR NO ENDEREÇO ?
-
+fim:
+    # Loop infinito
+    s.movh X
+    s.movl Y
+    s.brzr sr0, sr1
 
